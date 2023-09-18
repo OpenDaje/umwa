52a1a9531d825d25bd332ec9fa90b7e10c267c8f
zero <teclaizerai@gmail.com>
2023-09-18 15:04:54 +0200

    Improved Chats api. (post endpoint).

--- src/Api/Chats.php
+++ src/Api/Chats.php
@@ -36,4 +36,69 @@ class Chats extends AbstractApi
             'limit' => $limit,
         ]);
     }
+
+    /**
+     * Archive chat from chat list
+     *
+     * @see https://docs.ultramsg.com/api/post/chats/archive
+     */
+    public function archiveChat(string $chatId): array|string
+    {
+        return $this->postRaw('/' . rawurlencode($this->getInstanceId()) . '/chats/archive', http_build_query([
+            'token' => $this->getToken(),
+            'chatId' => $chatId,
+        ]));
+    }
+
+    /**
+     * Return archived chat to chat list
+     *
+     * @see https://docs.ultramsg.com/api/post/chats/unarchive
+     */
+    public function unarchiveChat(string $chatId): array|string
+    {
+        return $this->postRaw('/' . rawurlencode($this->getInstanceId()) . '/chats/unarchive', http_build_query([
+            'token' => $this->getToken(),
+            'chatId' => $chatId,
+        ]));
+    }
+
+    /**
+     * Clear all messages from the chat
+     *
+     * @see https://docs.ultramsg.com/api/post/chats/clearMessages
+     */
+    public function clearMessages(string $chatId): array|string
+    {
+        return $this->postRaw('/' . rawurlencode($this->getInstanceId()) . '/chats/clearMessages', http_build_query([
+            'token' => $this->getToken(),
+            'chatId' => $chatId,
+        ]));
+    }
+
+    /**
+     * Delete chat from chat list
+     *
+     * @see https://docs.ultramsg.com/api/post/chats/delete
+     */
+    public function deleteChat(string $chatId): array|string
+    {
+        return $this->postRaw('/' . rawurlencode($this->getInstanceId()) . '/chats/delete', http_build_query([
+            'token' => $this->getToken(),
+            'chatId' => $chatId,
+        ]));
+    }
+
+    /**
+     * Make chat messages as read for specific conversation
+     *
+     * @see https://docs.ultramsg.com/api/post/chats/read
+     */
+    public function markAsReaded(string $chatId): array|string
+    {
+        return $this->postRaw('/' . rawurlencode($this->getInstanceId()) . '/chats/read', http_build_query([
+            'token' => $this->getToken(),
+            'chatId' => $chatId,
+        ]));
+    }
 }
