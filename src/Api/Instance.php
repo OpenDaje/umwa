a4b13fd3096f7e27955fe0fa60489b5956ee5b09
zero <teclaizerai@gmail.com>
2023-09-18 02:01:43 +0200

    Added method Instance::reset(), refactoring.

--- src/Api/Instance.php
+++ src/Api/Instance.php
@@ -8,59 +8,64 @@ class Instance extends AbstractApi
      * Get the instance status
      *
      * @see https://docs.ultramsg.com/api/get/instance/status
-     *
-     * @return array|string
      */
-    public function getStatus(string $instanceId)
+    public function getStatus(): array|string
     {
-        return $this->get('/' . rawurlencode($instanceId) . '/instance/status');
+        return $this->get('/' . rawurlencode($this->getInstanceId()) . '/instance/status');
     }
 
     /**
      * Get instance settings
      *
      * @see https://docs.ultramsg.com/api/get/instance/settings
-     *
-     * @return array|string
      */
-    public function getSettings(string $instanceId)
+    public function getSettings(): array|string
     {
-        return $this->get('/' . rawurlencode($instanceId) . '/instance/settings');
+        return $this->get('/' . rawurlencode($this->getInstanceId()) . '/instance/settings');
     }
 
     /**
      * Get QR image for authentication
      *
      * @see https://docs.ultramsg.com/api/get/instance/qr
-     *
-     * @return array|string
      */
-    public function getQR(string $instanceId)
+    public function getQR(): array|string
     {
-        return $this->get('/' . rawurlencode($instanceId) . '/instance/qr');
+        return $this->get('/' . rawurlencode($this->getInstanceId()) . '/instance/qr');
     }
 
     /**
      * Get QR image for authentication
      *
      * @see https://docs.ultramsg.com/api/get/instance/qrCode
-     *
-     * @return array|string
      */
-    public function getQrCode(string $instanceId)
+    public function getQrCode(): array|string
     {
-        return $this->get('/' . rawurlencode($instanceId) . '/instance/qrCode');
+        return $this->get('/' . rawurlencode($this->getInstanceId()) . '/instance/qrCode');
     }
 
     /**
      * Get connected phone informations
      *
      * @see https://docs.ultramsg.com/api/get/instance/me
-     *
-     * @return array|string
      */
-    public function getConnectedPhoneInfo(string $instanceId)
+    public function getConnectedPhoneInfo(): array|string
     {
-        return $this->get('/' . rawurlencode($instanceId) . '/instance/me');
+        return $this->get('/' . rawurlencode($this->getInstanceId()) . '/instance/me');
+    }
+
+    /**
+     * Restart your WhatsApp instance.
+     *
+     * @see https://docs.ultramsg.com/api/post/instance/restart
+     */
+    public function restart(): array|string
+    {
+        return $this->postRaw(
+            '/' . rawurlencode($this->getInstanceId()) . '/instance/restart',
+            http_build_query([
+                'token' => $this->getToken(),
+            ])
+        );
     }
 }
