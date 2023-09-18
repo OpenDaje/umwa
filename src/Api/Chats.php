6ce72852317217976645032fe1ce47776f0db299
zero <teclaizerai@gmail.com>
2023-09-18 13:24:01 +0200

    Improved Chats api. (get endpoint).

--- src/Api/Chats.php
+++ src/Api/Chats.php
@@ -13,4 +13,27 @@ class Chats extends AbstractApi
     {
         return $this->get('/' . rawurlencode($this->getInstanceId()) . '/chats');
     }
+
+    /**
+     * Get the chats id's
+     *
+     * @see https://docs.ultramsg.com/api/get/chats/ids
+     */
+    public function getChatsIds(bool $clearIds = false): array|string
+    {
+        return $this->get('/' . rawurlencode($this->getInstanceId()) . '/chats/ids', [
+            'clear' => $clearIds,
+        ]);
+    }
+
+    /**
+     * Get last message from chat conversation
+     */
+    public function getChatMessages(string $chatId, int $limit = 50): array|string
+    {
+        return $this->get('/' . rawurlencode($this->getInstanceId()) . '/chats/messages', [
+            'chatId' => $chatId,
+            'limit' => $limit,
+        ]);
+    }
 }
